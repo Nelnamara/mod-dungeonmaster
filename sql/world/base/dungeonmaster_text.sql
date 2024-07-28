4a6c5c7884a2ad05bdaa11470e4c61cebef4cd29
Greg Wilkinson <gwilkinson@gmail.com>
2024-07-28 11:52:48 -0700

    Update dungeonmaster_text.sql
    Change ID and text.
    (Book Tribute)

--- sql/world/base/dungeonmaster_text.sql
+++ sql/world/base/dungeonmaster_text.sql
@@ -1,6 +1,6 @@
 
-SET @TEXT_ID = 197760,
-@TEXT= 'You want to go to a dungeon? I have explored all over the world and can get you where you need to go. I am not responsible for any death or dismemberment that may occure by going where you shouldn\'t';
+SET @TEXT_ID = 601021,
+@TEXT= 'You want to go to a dungeon? I have explored all over the world and can get you where you need to go. The Snorent Corporation is not responsible for any death or dismemberment that may occur by going where you shouldn\'t.  Now get out there... and kill, kill, KILL!';
 
 
 
